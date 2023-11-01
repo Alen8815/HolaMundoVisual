82950c5a472bf36a9c5e96368731e2a274eb9ad2
Alen8815 <alen.pineda536@ist17dejulio.edu.ec>
2023-11-01 14:48:50 -0500

    division , suma , resta

---
+++ src/holamundo_visual/HolaMundo_Visual.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo_visual;
+
+/**
+ *
+ * @author 59399
+ */
+public class HolaMundo_Visual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
